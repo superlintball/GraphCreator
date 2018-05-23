ce46a468ff61b5b25a23b7001ffdfef2381fa0f3
Raveen <ravnikator@gmail.com>
2018-05-22 20:19:21 -0700

    25 points

--- main.cpp
+++ main.cpp
@@ -19,19 +19,22 @@ void print(char* vertices[20], int adjacency[20][20])
 		{
 			//only put vertex names along the first line
 			if(i == -1)
-			{
-				cout << "\t";
-				
+			{				
 				if(vertices[j])
-					cout << vertices[j];
+					cout << "\t" << vertices[j];
 			}
 			//put the edge weight in the main body of the table
 			else
 			{
-				if(adjacency[i][j] != -1)
-					cout << adjacency[i][j];
-				else
+				if(vertices[i] && vertices[j])
+				{
+					if(adjacency[i][j] != -1)
+						cout << adjacency[i][j];
+					else
+						cout << "-";
+					
 					cout << "\t";
+				}
 			}
 		}
 		
@@ -48,20 +51,164 @@ void addVertex(char* vertices[20])
 	while(vertices[++index]);
 	
 	//prompt the user for the name and add it
-	cout << "Enter the name of the vertex you would like to add\n";
+	cout << "Enter the name of the vertex you would like to add.\n";
 	char* input = new char[15];
 	cin >> input;
 	vertices[index] = input;
 }
 
+//add an edge connecting vertices
 void addEdge(char* vertices[20], int adjacency[20][20])
 {
 	char* first = new char[15];
+	int firstInd = -1;
 	char* second = new char[15];
+	int secondInd = -1;
 	int weight = -1;
 	
-	cout << "Enter the name of the first vertex you want to connect\n";
+	//prompt the user for the vertices
+	cout << "Enter the name of the first vertex you want to connect.\n";
 	cin >> first;
+	cout << "Enter the name of the second vertex you want to connect.\n";
+	cin >> second;
+	
+	//find the index of each vertex
+	for(int i = 0; i < 20; i++)
+	{
+		if(vertices[i])
+		{
+			if(!strcmp(first, vertices[i]))
+				firstInd = i;
+			
+			if(!strcmp(second, vertices[i]))
+				secondInd = i;
+		}
+	}
+	
+	//prompt the user for the weight of the edge
+	cout << "Enter the weight of the edge you want to make.\n";
+	cin >> weight;
+	
+	//complain if something is wrong
+	if(firstInd == -1 || secondInd == -1 || weight == -1)
+	{
+		cout << "Something went wrong.\n";
+		return;
+	}
+	
+	//add the edge to the matrix
+	adjacency[firstInd][secondInd] = weight;
+	adjacency[secondInd][firstInd] = weight;
+}
+
+//remove a vertex and delete everything it's connected to
+void removeVertex(char* vertices[20], int adjacency[20][20])
+{
+	//prompt the user for the vertex to be removed
+	cout << "Enter the name of the vertex you would like to remove.\n";
+	char* input = new char[15];
+	cin >> input;
+	
+	//go through the list of vertices to find it
+	for(int i = 0; i < 20; i++)
+	{
+		if(vertices[i])
+		{
+			//if the vertex is found
+			if(!strcmp(input, vertices[i]))
+			{
+				//delete it
+				delete vertices[i];
+				vertices[i] = NULL;
+				
+				//set all its edges to 0
+				for(int j = 0; j < 20; j++)
+				{
+					adjacency[i][j] = -1;
+					adjacency[j][i] = -1;
+				}
+				
+				return;
+			}
+		}
+	}
+	
+	//if it wasn't found, tell the user
+	cout << "That vertex could not be found.\n";
+}
+
+//deletes an edge from the graph
+void removeEdge(char* vertices[20], int adjacency[20][20])
+{
+	char* first = new char[15];
+	int firstInd = -1;
+	char* second = new char[15];
+	int secondInd = -1;
+	
+	//prompt the user for the ends of the edge being deleted
+	cout << "Enter the name of one end of the edge you want to delete.\n";
+	cin >> first;
+	cout << "Enter the name of the other end of the edge you want to delete.\n";
+	cin >> second;
+	
+	//find the index of each vertex
+	for(int i = 0; i < 20; i++)
+	{
+		if(vertices[i])
+		{
+			if(!strcmp(first, vertices[i]))
+				firstInd = i;
+			
+			if(!strcmp(second, vertices[i]))
+				secondInd = i;
+		}
+	}
+	
+	//complain if something is wrong
+	if(firstInd == -1 || secondInd == -1 || adjacency[firstInd][secondInd] == -1)
+	{
+		cout << "Something doesn't exist here.\n";
+		return;
+	}
+	
+	//remove the edge from the matrix
+	adjacency[firstInd][secondInd] = -1;
+	adjacency[secondInd][firstInd] = -1;
+}
+
+//find the shortest path between two nodes
+void salesman(char* vertices[20], int adjacency[20][20])
+{
+	char* start = new char[15];
+	int startInd = -1;
+	char* end = new char[15];
+	int endInd = -1;
+	
+	//prompt the user for the ends of the edge being deleted
+	cout << "Enter the name of the vertex you want to start at.\n";
+	cin >> start;
+	cout << "Enter the name of the vertex you want to end at.\n";
+	cin >> end;
+	
+	//find the index of each vertex
+	for(int i = 0; i < 20; i++)
+	{
+		if(vertices[i])
+		{
+			if(!strcmp(start, vertices[i]))
+				startInd = i;
+			
+			if(!strcmp(end, vertices[i]))
+				endInd = i;
+		}
+	}
+	
+	//complain if something is wrong
+	if(startInd == -1 || endInd == -1)
+	{
+		cout << "Either the start or the end doesn't exist.\n";
+		return;
+	}
 }
 
 int main()
@@ -81,16 +228,35 @@ int main()
 			adjacency[i][j] = -1;
 		}
 	}
-	print(vertices, adjacency);
 	
-	addVertex(vertices);
-	addVertex(vertices);
-	addVertex(vertices);
-	addVertex(vertices);
+	bool running = true;
 	
-	adjacency[1][2] = 25;
-	
-	print(vertices, adjacency);	
+	while(running)
+	{
+		int input = 0;
+		cout << "Enter a number.\n1: Add a vertex to the graph\n2: Add an edge to the graph\n3: Remove a vertex from the graph\n";
+		cout << "4: Remove an edge from the graph\n5: Print the adjacency matrix\n6: Find the shortest distance between two vertices\n7: Quit\n\n";
+		cin >> input;
+		cout << endl;
+		if(input == 1)
+			addVertex(vertices);
+		else if(input == 2)
+			addEdge(vertices, adjacency);
+		else if(input == 3)
+			removeVertex(vertices, adjacency);
+		else if(input == 4)
+			removeEdge(vertices, adjacency);
+		else if(input == 5)
+			print(vertices, adjacency);
+		else if(input == 6)
+		{}
+		else if(input == 7)
+			running = false;
+		else
+			cout << "Sorry, I couldn't understand that.\n";
+		
+		cout << endl;
+	}
 	
 	return 0;
 }
